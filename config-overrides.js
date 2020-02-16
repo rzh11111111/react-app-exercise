ac19ecf01b4e434a14e48e605ae5aea95b5bec94
rzh11111111 <294346639@qq.com>
2020-02-16 17:34:09 +0800

    配置装饰器

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,14 @@
-const {override , fixBabelImports} = require('customize-cra');
+const {override , 
+    fixBabelImports,
+    addDecoratorsLegacy
+
+} = require('customize-cra');
 
 module.exports = override(
     fixBabelImports("import",{//antd按需加载
         libraryName:'antd',
         libraryDirectory:'es',
         style:'css'
-    })
+    }),
+    addDecoratorsLegacy(), //配置装饰器
 );
