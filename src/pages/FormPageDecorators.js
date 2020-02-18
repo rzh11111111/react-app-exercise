584484ee495287edfe7b172892286047c8bbf91c
rzh11111111 <294346639@qq.com>
2020-02-18 15:01:17 +0800

    校验

--- src/pages/FormPageDecorators.js
+++ src/pages/FormPageDecorators.js
@@ -2,11 +2,30 @@ import React, {Component} from 'react';
 import {Form,Input,Icon, Button } from 'antd'
 const FormItem=Form.Item
 
+
+const nameRule = {
+    required:true,
+    message:'please input your name'
+}
+
+const passwordRule = {
+    required:true,
+    message:'please input your password'
+}
+
 class FormPageDecorators extends Component {
     submit=()=>{
         //antd封装的方法
-        const {getFieldsValue,getFieldValue} = this.props.form
-        console.log('submie',getFieldsValue(),getFieldValue("name"));
+        const {getFieldsValue,getFieldValue,validateFields} = this.props.form
+
+        validateFields((err,value)=>{
+            if(err){
+                console.log('err',err)
+            }else{
+                console.log('submit',value)
+            }
+        })
+        // console.log('submie',getFieldsValue(),getFieldValue("name"));
     }
     render(){
         const {getFieldDecorator} = this.props.form
@@ -17,11 +36,11 @@ class FormPageDecorators extends Component {
                 <Form>
                     <FormItem label="姓名">
                         {/* 高阶组件 */}
-                        {getFieldDecorator("name")(<Input prefix={<Icon type="user"/>}/>)}
+                        {getFieldDecorator("name",{rules:[nameRule]})(<Input prefix={<Icon type="user"/>}/>)}
                         
                     </FormItem>
                     <FormItem label="密码">
-                        {getFieldDecorator("password")(<Input type="password"  prefix={<Icon type="lock"/>}/>)}
+                        {getFieldDecorator("password",{rules:[passwordRule]})(<Input type="password"  prefix={<Icon type="lock"/>}/>)}
                         
                     </FormItem>
                     <FormItem >
