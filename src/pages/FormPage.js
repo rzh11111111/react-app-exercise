f4fd5d88214f3207d4d219cec5b53b7200ef478d
rzh11111111 <294346639@qq.com>
2020-02-18 13:11:51 +0800

    简单表单

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -1,10 +1,23 @@
 import React,{Component} from 'react';
+import {Form,Input,Icon, Button } from 'antd'
+const FormItem=Form.Item
 
 export default class FormPage extends Component {
     render(){
         return (
             <div>
                 FormPage
+                <Form>
+                    <FormItem label="姓名">
+                        <Input prefix={<Icon type="user"/>}/>
+                    </FormItem>
+                    <FormItem label="密码">
+                        <Input type="password" prefix={<Icon type="lock"/>}/>
+                    </FormItem>
+                    <FormItem >
+                        <Button>提交</Button>
+                    </FormItem>
+                </Form>
             </div>
         )
     }
