81c2fce27fc3a9e8aa81e25d6e7539ccafacf7a7
rzh11111111 <294346639@qq.com>
2020-02-18 13:21:58 +0800

    react获取值

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -3,19 +3,35 @@ import {Form,Input,Icon, Button } from 'antd'
 const FormItem=Form.Item
 
 export default class FormPage extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            name:'',
+            password:''
+        };
+    }
+    change=(field,event)=>{
+        this.setState({
+            [field]:event.target.value
+        })
+    }
+    submit=()=>{
+        console.log('submie',this.state)
+    }
     render(){
+        // const {name,password}=this.state
         return (
             <div>
                 FormPage
                 <Form>
                     <FormItem label="姓名">
-                        <Input prefix={<Icon type="user"/>}/>
+                        <Input prefix={<Icon type="user"/>} onChange={(event)=>this.change('name',event)}/>
                     </FormItem>
                     <FormItem label="密码">
-                        <Input type="password" prefix={<Icon type="lock"/>}/>
+                        <Input type="password" prefix={<Icon type="lock"/>} onChange={(event)=>this.change('password',event)}/>
                     </FormItem>
                     <FormItem >
-                        <Button>提交</Button>
+                        <Button onClick={this.submit}>提交</Button>
                     </FormItem>
                 </Form>
             </div>
