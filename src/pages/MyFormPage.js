876ea6196f7e25cd4859ba468b3f926907d7c69c
rzh11111111 <294346639@qq.com>
2020-02-19 15:46:56 +0800

    表单

--- src/pages/MyFormPage.js
+++ src/pages/MyFormPage.js
@@ -4,28 +4,77 @@ import React , {Component} from 'react';
 //高阶组件
 function kFormCreate(Cmp) {
     return class extends Component {
+        constructor(props){
+            super(props)
+            this.options={}//存储配置字段项
+            this.state={}//存字段
+        }
+        handleChange=(event)=>{
+            const {name,value} = event.target
+            this.setState({
+                [name]:value
+            })
+        }
+        getFieldDecorator=(field,option)=>{
+            this.options[field]=option;
+            return InputCmp=><div className="border">{
+                React.cloneElement(InputCmp,{
+                    name:field,
+                    value:this.state[field]||"",
+                    onChange:this.handleChange
+                })
+            }</div>
+        }
+        getFieldsValue=()=>{
+            return {...this.state}
+        }
         render(){
             return (
                 <div className="border">
-                    <Cmp {...this.props}/>
+                    <Cmp {...this.props} getFieldDecorator={this.getFieldDecorator}
+                    getFieldsValue={this.getFieldsValue}
+                    />
+                    
                 </div>
             )
         }
     }
 }
 
-export default class MyFormPage extends Component {
+class MyFormPage extends Component {
     submit =()=>{
-        console.log("submit")
+        const {getFieldsValue} = this.props
+        console.log("submit",getFieldsValue())
     }
     render(){
+        const {getFieldDecorator} = this.props
         return (
             <div>
                 MyFormPage
-                <input/>
-                <input type="password"/>
-                <button onClick={this.submit}>提交</button>
+                
+                       
+                        {
+                        getFieldDecorator('name')(
+                            <input type="text"/>
+                        )
+                        
+                        }
+
+                        {
+                        getFieldDecorator('password')(
+                            <input type="password"/>
+                        )
+                        
+                        }
+                        
+                        
+                   
+                        <button onClick={this.submit}>提交</button>
+                    
+                
             </div>
         )
     }
 }
+
+export default kFormCreate(MyFormPage)
