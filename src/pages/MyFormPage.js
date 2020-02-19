2bb04d02eda583e661f0c2217e5deefc760f17ca
rzh11111111 <294346639@qq.com>
2020-02-19 14:51:43 +0800

    kFormCreate高阶组件

--- src/pages/MyFormPage.js
+++ src/pages/MyFormPage.js
@@ -1,10 +1,30 @@
 import React , {Component} from 'react';
 //自己实现formpage
+
+//高阶组件
+function kFormCreate(Cmp) {
+    return class extends Component {
+        render(){
+            return (
+                <div className="border">
+                    <Cmp {...this.props}/>
+                </div>
+            )
+        }
+    }
+}
+
 export default class MyFormPage extends Component {
+    submit =()=>{
+        console.log("submit")
+    }
     render(){
         return (
             <div>
                 MyFormPage
+                <input/>
+                <input type="password"/>
+                <button onClick={this.submit}>提交</button>
             </div>
         )
     }
