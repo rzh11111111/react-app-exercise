85e615706b13ebf21f1a7b73c678d11d3b97d773
rzh11111111 <294346639@qq.com>
2020-02-20 17:13:59 +0800

    删除全局创建的div

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -9,6 +9,10 @@ export default class Dialog extends Component{
         this.node = doc.createElement('div')
         doc.body.appendChild(this.node)
     }
+    //删除全局创建的div
+    componentWillUnmount(){
+        window.document.body.removeChild(this.node)
+    }
     render(){
         return createPortal(
             <div className="dialog">
