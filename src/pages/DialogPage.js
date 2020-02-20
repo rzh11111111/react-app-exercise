d0a412d12ce20b3b005b0a9059cac6c6cb6426fd
rzh11111111 <294346639@qq.com>
2020-02-20 16:21:50 +0800

    弹窗

---
+++ src/pages/DialogPage.js
@@ -0,0 +1,11 @@
+import React , {Component} from 'react';
+
+export default class DialogPage extends Component {
+    render(){
+        return (
+            <div>
+                DialogPage
+            </div>
+        )
+    }
+}
