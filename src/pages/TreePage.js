6dcf56e84245b028dc8a2a3439bbda5824de571f
rzh11111111 <294346639@qq.com>
2020-02-21 21:21:34 +0800

    TreePage

---
+++ src/pages/TreePage.js
@@ -0,0 +1,11 @@
+import React , {Component} from 'react';
+
+export default class TreePage extends Component {
+    render(){
+        return (
+            <div>
+                TreePage
+            </div>
+        )
+    }
+}
