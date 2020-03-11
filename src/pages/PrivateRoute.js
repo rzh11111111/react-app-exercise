812c58518cb5252e2643d243f17045122c05932c
rzh11111111 <294346639@qq.com>
2020-03-11 21:01:06 +0800

    判断登录

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 
 class PrivateRoute extends Component {
   render() {
+    // isLogin来自props
     const { component: Cmp, isLogin, ...rest } = this.props;
     console.log("PrivateRoute", this.props);
     return (
@@ -11,8 +12,10 @@ class PrivateRoute extends Component {
         {...rest}
         render={props => {
           return isLogin ? (
+            // 如果登录了的话
             <Cmp {...props} />
           ) : (
+            //如果没登录
             <Redirect
               to={{
                 pathname: "/login",
