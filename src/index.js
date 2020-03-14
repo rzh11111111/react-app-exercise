389331858fae240651de66e533c40f77356a28bf
rzh11111111 <294346639@qq.com>
2020-03-14 22:31:37 +0800

    引入store

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import * as serviceWorker from './serviceWorker';
 
 
 
-import store from './store/reactReduxStore'
+import store from './store/'
 import {Provider} from 'react-redux'
 ReactDOM.render(
     <Provider store={store}>
